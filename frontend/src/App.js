d4d45a4fbd5fa20fe394cf6affb205ff1630c059
Richa Singh <singhricha0724@gmail.com>
2020-11-24 00:32:36 +0530

    Files updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom'
+import Add from './Pages/Add';
+import Dashboard from "./Pages/Dashboard"
+import Edit from "./Pages/Edit"
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route path="/" exact render={(props) => <Dashboard {...props} />} />
+      <Route path="/:email" render={(props) => <Edit {...props} />} />
+      <Route path="/add" render={(props) => <Add {...props} />} />
+    </>
   );
 }
 
